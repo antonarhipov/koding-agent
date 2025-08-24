58ca3844e9cfc7115a212bf3728cb70e39ab17d6
antonarhipov <ant.arhipov@gmail.com>
2025-08-24 20:19:39 +0300

    autoselect for Ollama-hosted models + tracing

--- src/main/kotlin/org/example/demo/demo_01_simple_tracing.kt
+++ src/main/kotlin/org/example/demo/demo_01_simple_tracing.kt
@@ -6,31 +6,12 @@ import ai.koog.agents.ext.tool.SayToUser
 import ai.koog.agents.features.common.message.FeatureMessage
 import ai.koog.agents.features.common.message.FeatureMessageProcessor
 import ai.koog.agents.features.tracing.feature.Tracing
-import ai.koog.prompt.executor.clients.openai.OpenAIModels
-import ai.koog.prompt.executor.llms.SingleLLMPromptExecutor
-import ai.koog.prompt.executor.llms.all.simpleOpenAIExecutor
-import ai.koog.prompt.executor.ollama.client.OllamaClient
-import ai.koog.prompt.executor.ollama.client.toLLModel
-import com.sun.tools.javac.tree.TreeInfo.args
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
     runBlocking {
         //select executor based on command line parameter
-        val (executor, model) = when (args.firstOrNull() ?: "mistral") {
-            "openai" -> {
-                val openAIApiToken = System.getenv("OPENAI_API_KEY") ?: error("OPENAI_API_KEY environment variable not set")
-                simpleOpenAIExecutor(openAIApiToken) to OpenAIModels.Chat.GPT4o
-            }
-
-            "mistral" -> {
-                val client = OllamaClient()
-                val model = runBlocking { client.getModelOrNull("mistral")!!.toLLModel() }
-                SingleLLMPromptExecutor(client) to model
-            }
-
-            else -> throw IllegalArgumentException("Invalid argument: ${args.firstOrNull()}")
-        }
+        val (executor, model) = autoselect("gpt-oss:20b")
 
         val toolRegistry = ToolRegistry {
             tools(
@@ -48,15 +29,15 @@ fun main(args: Array<String>) {
             llmModel = model,
             toolRegistry = toolRegistry,
         ) {
-//        install(Tracing) {
-//            addMessageProcessor(object : FeatureMessageProcessor() {
-//                override suspend fun processMessage(message: FeatureMessage) = println(message)
-//                override suspend fun close() = TODO("Not yet implemented")
-//            })
-//        }
+            install(Tracing) {
+                addMessageProcessor(object : FeatureMessageProcessor() {
+                    override suspend fun processMessage(message: FeatureMessage) = println("$message\n")
+                    override suspend fun close() = TODO("Not yet implemented")
+                })
+            }
         }
 
-        agent.run("What are the main benefits of using Kotlin in server-side applications?")
+        agent.run("Tell me a friendly programmers' joke about Amsterdam?")
     }
 }
 
