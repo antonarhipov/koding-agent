ee1e69024232f0f59f598d37a9f392478ecf1621
antonarhipov <ant.arhipov@gmail.com>
2025-08-24 20:51:49 +0300

    autoselect for Ollama-hosted models + tool call

--- src/main/kotlin/org/example/demo/demo_03_strategy.kt
+++ src/main/kotlin/org/example/demo/demo_03_strategy.kt
@@ -3,49 +3,24 @@ package org.example.demo
 import ai.koog.agents.core.agent.AIAgent
 import ai.koog.agents.core.dsl.builder.forwardTo
 import ai.koog.agents.core.dsl.builder.strategy
-import ai.koog.agents.core.dsl.extension.nodeExecuteTool
-import ai.koog.agents.core.dsl.extension.nodeLLMRequest
-import ai.koog.agents.core.dsl.extension.nodeLLMSendToolResult
-import ai.koog.agents.core.dsl.extension.onAssistantMessage
-import ai.koog.agents.core.dsl.extension.onToolCall
+import ai.koog.agents.core.dsl.extension.*
 import ai.koog.agents.core.tools.ToolRegistry
 import ai.koog.agents.core.tools.ToolResult
 import ai.koog.agents.core.tools.annotations.LLMDescription
 import ai.koog.agents.core.tools.annotations.Tool
+import ai.koog.agents.core.tools.reflect.tool
 import ai.koog.agents.ext.tool.SayToUser
 import ai.koog.agents.features.common.message.FeatureMessage
 import ai.koog.agents.features.common.message.FeatureMessageProcessor
 import ai.koog.agents.features.tracing.feature.Tracing
-import ai.koog.prompt.executor.clients.openai.OpenAIModels
-import ai.koog.prompt.executor.llms.SingleLLMPromptExecutor
-import ai.koog.prompt.executor.llms.all.simpleOpenAIExecutor
-import ai.koog.prompt.executor.ollama.client.OllamaClient
-import ai.koog.prompt.executor.ollama.client.toLLModel
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
-import ai.koog.agents.core.tools.reflect.tool
-import ai.koog.prompt.llm.LLMCapability
-import ai.koog.prompt.llm.LLMProvider
-import ai.koog.prompt.llm.LLModel
 import org.example.kagent.createCodingAgentStrategy
 
 fun main(args: Array<String>) {
     runBlocking {
         //select executor based on command line parameter
-        val (executor, model) = when (args.firstOrNull() ?: "devstral") {
-            "openai" -> {
-                val openAIApiToken = System.getenv("OPENAI_API_KEY") ?: error("OPENAI_API_KEY environment variable not set")
-                simpleOpenAIExecutor(openAIApiToken) to OpenAIModels.Chat.GPT4o
-            }
-
-            "devstral" -> {
-                val client = OllamaClient()
-                val model = runBlocking { client.getModelOrNull("devstral")!!.toLLModel() }
-                SingleLLMPromptExecutor(client) to model
-            }
-
-            else -> throw IllegalArgumentException("Invalid argument: ${args.firstOrNull()}")
-        }
+        val (executor, model) = autoselect("gpt-oss:20b")
 
         val toolRegistry = ToolRegistry {
             tool(SayToUser)
@@ -61,26 +36,28 @@ fun main(args: Array<String>) {
             2. Execute tools to perform the changes.
             3. Do not engage in conversation before completing the task.
             4. Say the final result to the user.
-
+            
             MANDATORY INSTRUCTIONS:
             - Only use provided tools
             - No direct conversation - complete task first
             - All changes must be made through tool execution
             - You must use the SAYTOOL to provide the result to the user
         """.trimIndent(),
+            strategy = createStrategy(),
             llmModel = model,
             toolRegistry = toolRegistry,
-            strategy = createCodingAgentStrategy(),
-        ) {
-            install(Tracing) {
-                addMessageProcessor(object : FeatureMessageProcessor() {
-                    override suspend fun processMessage(message: FeatureMessage) = println(message)
-                    override suspend fun close() = TODO("Not yet implemented")
-                })
-            }
-        }
+        )
+//        {
+//            install(Tracing) {
+//                addMessageProcessor(object : FeatureMessageProcessor() {
+//                    override suspend fun processMessage(message: FeatureMessage) = println(message)
+//                    override suspend fun close() = TODO("Not yet implemented")
+//                })
+//            }
+//        }
 
-        val mathProblemToSolve = "I had 5 apples. I ate 3 apples. My friend gave me 2 more apples. How many apples do I have left?"
+        val mathProblemToSolve =
+            "I had 5 apples. I ate 3 apples. My friend gave me 2 more apples. How many apples do I have left?"
         agent.run(mathProblemToSolve)
     }
 }
@@ -98,11 +75,10 @@ suspend fun mathTool(
     numbers: List<Int>,
 ): MathResult {
     println(">>>>>>>>>>>> TRACE: mathTool " + numbers.joinToString())
-  return MathResult(numbers.sumOf { it })  
-} 
+    return MathResult(numbers.sumOf { it })
+}
 
-
-fun createCodingAgentStrategy() = strategy("Assistant Workflow") {
+fun createStrategy() = strategy("Assistant Workflow") {
     // Define nodes for the coding workflow
     val nodeAnalyzeRequest by nodeLLMRequest()
     val nodeExecuteTool by nodeExecuteTool()
