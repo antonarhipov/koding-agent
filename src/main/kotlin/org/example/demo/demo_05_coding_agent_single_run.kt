8c9a1159227e6ac0befb92497fb8f4fef08df6a9
antonarhipov <ant.arhipov@gmail.com>
2025-12-05 00:46:50 +1100

    simple coding agent

---
+++ src/main/kotlin/org/example/demo/demo_05_coding_agent_single_run.kt
@@ -0,0 +1,51 @@
+package org.example.demo
+
+import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.core.agent.singleRunStrategy
+import ai.koog.agents.core.tools.ToolRegistry
+import ai.koog.agents.ext.tool.file.EditFileTool
+import ai.koog.agents.ext.tool.file.ListDirectoryTool
+import ai.koog.agents.ext.tool.file.ReadFileTool
+import ai.koog.agents.features.eventHandler.feature.handleEvents
+import ai.koog.rag.base.files.JVMFileSystemProvider
+
+
+suspend fun main(args: Array<String>) {
+//    val (executor, model) = openai()
+    val (executor, model) = gptoss()
+
+//val executor = simpleOpenAIExecutor(System.getenv("OPENAI_API_KEY"))
+    val agent = AIAgent(
+        promptExecutor = executor,
+        llmModel = model,
+        toolRegistry = ToolRegistry {
+            tool(ListDirectoryTool(JVMFileSystemProvider.ReadOnly))
+            tool(ReadFileTool(JVMFileSystemProvider.ReadOnly))
+            tool(EditFileTool(JVMFileSystemProvider.ReadWrite))
+            tool(createExecuteShellCommandToolFromEnv())
+        },
+        systemPrompt = """
+            You are a coding assistant helping the user to write programs according to their requests.
+            Implement the user request according to the supplied plan.
+            The code should be generated a dedicated directory.  
+            Once you have the answer, tell it to the user.
+        """.trimIndent(),
+        strategy = singleRunStrategy(),
+        maxIterations = 400
+    ) {
+        handleEvents {
+            onToolCallStarting { ctx ->
+                println("Tool '${ctx.tool.name}' called with args: ${ctx.toolArgs.toString().take(100)}")
+            }
+        }
+    }
+
+    val (path, task) = ("/Users/anton/IdeaProjects/kagent/demo" to "Write fizzbuzz program in Kotlin")
+    val input = "Project absolute path: $path\n\n## Task\n$task"
+    try {
+        val result = agent.run(input)
+        println(result)
+    } finally {
+        executor.close()
+    }
+}
