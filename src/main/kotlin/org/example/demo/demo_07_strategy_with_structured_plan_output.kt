7b0a835d067158f8e18bfd702c93a39a4e478d51
antonarhipov <ant.arhipov@gmail.com>
2025-12-05 12:47:02 +1100

    experimenting with structured output

--- src/main/kotlin/org/example/demo/demo_07_strategy_with_structured_plan_output.kt
+++ src/main/kotlin/org/example/demo/demo_07_strategy_with_structured_plan_output.kt
@@ -5,6 +5,7 @@ import ai.koog.agents.core.agent.config.AIAgentConfig
 import ai.koog.agents.core.agent.entity.ToolSelectionStrategy
 import ai.koog.agents.core.dsl.builder.strategy
 import ai.koog.agents.core.tools.ToolRegistry
+import ai.koog.agents.core.tools.annotations.LLMDescription
 import ai.koog.agents.ext.agent.subgraphWithTask
 import ai.koog.agents.ext.tool.SayToUser
 import ai.koog.agents.ext.tool.file.EditFileTool
@@ -12,19 +13,44 @@ import ai.koog.agents.ext.tool.file.ListDirectoryTool
 import ai.koog.agents.ext.tool.file.ReadFileTool
 import ai.koog.agents.features.eventHandler.feature.handleEvents
 import ai.koog.prompt.dsl.prompt
+import ai.koog.prompt.executor.clients.openai.OpenAIModels
+import ai.koog.prompt.executor.llms.Executors.promptExecutor
+import ai.koog.prompt.message.Message
+import ai.koog.prompt.structure.StructureFixingParser
+import ai.koog.prompt.structure.executeStructured
 import ai.koog.rag.base.files.JVMFileSystemProvider
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PlanItem(val id: Int, val task: String)
+@SerialName("Task")
+@LLMDescription("A single task in a task plan.")
+data class Task(
+    @property:LLMDescription("The task identifier")
+    val id: Int,
+    @property:LLMDescription("The task description")
+    val task: String)
 
 @Serializable
-data class Plan(
+@SerialName("TaskPlan")
+@LLMDescription("A sequential plan for implementing a user request, contains a list of tasks.")
+data class TaskPlan(
+    @property:LLMDescription("The task plan identifier")
     val id: Int,
-    val items: List<PlanItem>
+    @property:LLMDescription("The list of tasks in the plan")
+    val items: List<Task>
 )
 
+val examplePlan = listOf(
+    TaskPlan(1, listOf(
+        Task(1, "Create the project directory at /some/path"),
+        Task(2, "Initialize a new project "),
+        Task(3, "Create Foo.kt and implement the function"),
+        Task(4, "Create FooTest.kt and write unit tests that verify the function"),
+        Task(5, "Run test to ensure all tests pass")
+    ))
+)
 
 fun main(args: Array<String>) {
     runBlocking {
@@ -32,28 +58,44 @@ fun main(args: Array<String>) {
         val (executor, model) = openai()
 
         val codingStrategy = strategy<String, String>("coding strategy") {
-            val nodePlanWork by subgraphWithTask<String, Plan>(
+
+            val nodePlanWork by subgraphWithTask<String, String>(
                 name = "plan work",
                 tools = emptyList()) { input ->
                 """
                     Create a minimal list of tasks as a plan, how to implement the request.
                     Assume that this is a new project and all new code should be written in a new folder.
                     The first step in the plan should always be creating the new directory for the project.
-                    Enumerate the tasks. 
-                    
-                    Provide the plan in JSON format as follows: a JSON collection of JSON elements with id:Int and task:String fields.
+                    Enumerate the tasks.
                     
                     User input: $input
                 """
             }
 
+            val nodePlanWorkStructured by node<String, TaskPlan> { _ ->
+                val structuredResponse = llm.writeSession {
+                    requestLLMStructured<TaskPlan>(
+                        examples = examplePlan
+                    )
+                }
+
+                val default = TaskPlan(0, items = emptyList())
+
+                val plan = structuredResponse.getOrNull()?.data ?: default
+                println("I've got the structured plan! Number of tasks: ${plan.items.size}")
+
+                plan
+            }
+
+
             // This is unreliable. For better results, see https://docs.koog.ai/structured-output/
-            val nodeAnalyzePlan by node<Plan, Plan> { plan ->
+            val nodeAnalyzePlan by node<TaskPlan, TaskPlan> { plan ->
+                println("------- Implementation Plan --------")
                 plan.items.forEach { println(it) }
                 plan
             }
 
-            val nodeImplementTask by subgraphWithTask<Plan, String>(
+            val nodeImplementTask by subgraphWithTask<TaskPlan, String>(
                 ToolSelectionStrategy.ALL,
                 name = "implement task",
             ) { input ->
@@ -66,7 +108,7 @@ fun main(args: Array<String>) {
                 """
             }
 
-            nodeStart then nodePlanWork then nodeAnalyzePlan then nodeImplementTask then nodeFinish
+            nodeStart then nodePlanWork then nodePlanWorkStructured then nodeAnalyzePlan then nodeImplementTask then nodeFinish
         }
 
         val agent = AIAgent(
@@ -102,6 +144,13 @@ fun main(args: Array<String>) {
 
                 onToolCallStarting { ctx ->
                     println("Calling tool: ${ctx.tool.name}(${ctx.toolArgs})")
+                }
+                onLLMCallStarting { ctx ->
+                    println("LLM Call =========================  ")
+                    println("Prompt: ${ctx.prompt}")
+                    println("Tools: ")
+                    ctx.tools.forEach { println(it.name) }
+                    println("=======================================  ")
                 }
                 onLLMCallCompleted { ctx ->
                     println("LLM responded =========================  ")
